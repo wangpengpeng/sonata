cb5caab02c22dcde0477216502e0c251bc9b38e0
Richard Hoogstad <rshoogstad@gmail.com>
2015-07-08 22:36:33 +0200

    tweaks for quickstart script

--- README.md
+++ README.md
@@ -24,3 +24,5 @@ app/console doctrine:schema:create
 app/console sonata:page:create-site
 ```
 cd into /project-name/web and type bower install ../vendor/sonata-project/admin-bundle/bower.json
+
+you can also run the quickstart.bash from the console
--- quickstart.bash
+++ quickstart.bash
@@ -3,6 +3,7 @@ composer install
 chmod -R 777 app/logs app/cache
 app/console doctrine:database:create
 app/console doctrine:schema:create
+app/console doctrine:fixtures:load
 app/console sonata:page:create-site
 cd web
 bower install ../vendor/sonata-project/admin-bundle/bower.json
