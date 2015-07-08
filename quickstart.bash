5d8a9db864d5752aa9af42449305e08ff26fba3f
Richard Hoogstad <rshoogstad@gmail.com>
2015-07-08 22:21:47 +0200

    minor additions to automate setup

--- quickstart.bash
+++ quickstart.bash
@@ -3,3 +3,6 @@ composer install
 chmod -R 777 app/logs app/cache
 app/console doctrine:database:create
 app/console doctrine:schema:create
+app/console sonata:page:create-site
+cd web
+bower install ../vendor/sonata-project/admin-bundle/bower.json
